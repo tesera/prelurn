2422337d51f0fe408d7b57dc0633bb715c132b57
Jotham Apaloo <jotham.apaloo@tesera.com>
2016-04-28 18:27:20 -0700

    Setup tests for s3
    Add a skip decorator if s3 is not configured

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,13 +5,18 @@ from click.testing import CliRunner
 from prelurn.scripts.cli import cli
 
 
+skip_if_no_s3 = pytest.mark.skipif(os.environ.get('S3_TEST_DATA_URL') is None,
+                                   reason=('Environment variable for s3 data'
+                                           'is not configured'))
+
+# TODO: check data is correct
 def test_cli_describe(test_data_path):
     runner = CliRunner()
     result = runner.invoke(cli, ['describe', test_data_path])
 
     assert result.exit_code == 0
     assert os.path.exists('data_describe.csv')
-    # TODO: check data is correct
+
 
 def test_cli_describe_json(test_data_path):
     runner = CliRunner()
@@ -19,11 +24,19 @@ def test_cli_describe_json(test_data_path):
 
     assert result.exit_code == 0
     assert os.path.exists('data_describe.json')
-    # TODO: check data is correct values
+
+
+@skip_if_no_s3
+def test_cli_describe_with_s3_source():
+    test_data_path = sys.getenv('S3_TEST_DATA_URL', None)
+    runner = CliRunner()
+    result = runner.invoke(cli, ['describe', test_data_path, '-j'])
+
+    assert result.exit_code == 0
+    assert os.path.exists('data_describe.json')
 
 
 # some may go in module tests
-# test from s3
 # test s3 without credentials
 # test to s3
 # test only categorical data
